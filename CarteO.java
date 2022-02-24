238266967885b315a44d2f3ed17a559b5084921d
Malo30 <99038964+Malo30@users.noreply.github.com>
2022-02-24 17:20:42 +0100

    Update CarteO.java

--- CarteO.java
+++ CarteO.java
@@ -1,18 +1,28 @@
-
 public class CarteO  extends Carte{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Orientation o;
 	
-	public Orientation getO() {
-		return o;
-	}
-
-	public void setO(Orientation o) {
-		this.o = o;
-	}
-
 	public CarteO(String nom,String description, Orientation o) {
 		super(nom,description);
 		this.o = o;
-	}
 		
+	}
+	
+   public void effet(Bot r) {
+	   r.setO(o);
+   }
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
+
 }
