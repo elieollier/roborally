0f61d8bb2fd52170a7b343e156e076207a9b0b2c
Malo30 <99038964+Malo30@users.noreply.github.com>
2022-03-11 17:01:26 +0100

    Create Plateau.java

---
+++ Plateau.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
+
+
+public class Plateau {
+	
+
+Random  rand  = new Random();
+
+
+
+ public 	ArrayList<Case> remplissage(){
+	 ArrayList <Case> lis_carte =  new ArrayList <Case>();
+	lis_carte.add(new CaseBroyeur());
+	lis_carte.add(new CaseCheckpoint(0));
+	lis_carte.add(new CaseNormale());
+	lis_carte.add(new CaseTapisRoulant());
+	lis_carte.add(new CaseTrou());
+	return lis_carte;
+	}
+ 
+	
+public Plateau() {
+
+ Case cases[][] = new Case[12][12] ;
+ ArrayList <Case> lis_carte = remplissage();
+ 
+ for (int i =0 ; i < 12; i++) {
+	 for (int j = 0; j < 12; j++) {
+		 cases[i][j] = lis_carte.get(rand.nextInt(5));
+		 
+	 }
+ }
+ 
+
+ 
+ 
+	}
+
+}
