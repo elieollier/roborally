f8326a6342d9ec8f7541b202eebd64c79eb22beb
Malo30 <99038964+Malo30@users.noreply.github.com>
2022-03-27 19:59:10 +0200

    Update CaseTapisRoulant.java

--- CaseTapisRoulant.java
+++ CaseTapisRoulant.java
@@ -2,19 +2,22 @@ public class CaseTapisRoulant implements Case{
 
 
 public class CaseTapisRoulant implements Case {
-	  Orientation orientations [] =  {Orientation.haut,Orientation.bas,Orientation.gauche,Orientation.droite};
-	  Random alea = new Random();
+		   private Orientation o ;
+	   
+	   public CaseTapisRoulant(Orientation o) {
+		   this.o = o;
+		   
+		   
+	   }
 	
-	public void effet(Bot r) {
-	  Orientation o = orientations[alea.nextInt(4)];
-		if ( o == Orientation.droite)
-			r.setX( r.getX()+ 1);
-		else if(o== Orientation.gauche)
-			r.setX( r.getX()- 1);
-		else if (o == Orientation.haut)
-			r.setY( r.getY()- 1);
-		else
-			r.setY( r.getY()+ 1);
-}
-	
+	   
+	   public void effet(Bot r) {
+		   if( r.getO() != o) {
+			   r.mourir();
+		   }
+		   else {
+			   r.deplacement();
+		   }
+		   
+	   }
 }
