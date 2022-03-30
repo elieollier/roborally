e1f20dbce4b46273e3433c78c1b70151036f5b36
ThomasAyr <99038943+ThomasAyr@users.noreply.github.com>
2022-03-31 00:25:32 +0200

    Update CarteD.java

--- CarteD.java
+++ CarteD.java
@@ -1,41 +1,28 @@
+import java.util.Random;
+
 public class CarteD  extends Carte{
+	private static final long serialVersionUID = 1L;
+	
+	private String nom;
+	private int deplacement;
 	private int vitesse;
 	
-	
-	public CarteD( int vitesse) {
-		setVitesse(vitesse);
+	public CarteD(int deplacement) {
+		this.deplacement  = deplacement;
+		this.nom = "deplacement"+String.valueOf(deplacement);
+		this.vitesse = new Random().nextInt(500);
 	}
-
-
+	
+	public String getNom() {
+		return nom;
+	}
+	 
 	public int getVitesse() {
 		return vitesse;
 	}
 
-
-	public void setVitesse(int vitesse) {
-		this.vitesse = vitesse;
-	}
-	
-
-	public void effet(Bot r, int deplacement) {
-		
-					
-		for (int i =0 ; i < deplacement; i++) {
-				
+	public void effet(Bot r) {
+		for (int i =0 ; i < deplacement; i++) {	
 			r.deplacement();}
 		}
-	
-	public void effet(Bot r) {
-		
-	}
-		
-		
-		
-		
-		
-	
-
-
-
-
 }
