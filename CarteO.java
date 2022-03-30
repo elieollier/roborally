bc59e0f7505e7c98b4832ecf1392450801d10536
ThomasAyr <99038943+ThomasAyr@users.noreply.github.com>
2022-03-31 00:32:09 +0200

    Update CarteO.java

--- CarteO.java
+++ CarteO.java
@@ -1,28 +1,54 @@
-public class CarteO  extends Carte{
-private static final long serialVersionUID = 1L;
+public class CarteO extends Carte{
+	private static final long serialVersionUID = 1L;
+	
+	private String nom;
 	private Orientation o;
 	
-	public CarteO(String nom,String description, Orientation o) {
-		super(nom,description);
+	public CarteO(Orientation o) {
 		this.o = o;
-		
+		this.nom = String.valueOf(o);
 	}
 	
-   public void effet(Bot r) {
-	   r.setO(o);
-   }
-	
-	
-	public void effet(Bot r, int depalcement) {
-		
+   public String getNom() {
+		return nom;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
+   public Orientation getO() {
+		return o;
+   }
+
+   public void effet(Bot r) {
+	   if(o == Orientation.gauche) {
+			switch(r.getO()) {
+				case haut:
+					r.setO(Orientation.droite);
+					break;
+				case bas:
+					r.setO(Orientation.gauche);
+					break;
+				case droite:
+					r.setO(Orientation.bas);
+					break;
+				case gauche:
+					r.setO(Orientation.haut);
+					break;
+			}
+	   }
+	   else { //(o == Orientation.droite)
+		   switch(r.getO()) {
+				case haut:
+					r.setO(Orientation.gauche);
+					break;
+				case bas:
+					r.setO(Orientation.droite);
+					break;
+				case droite:
+					r.setO(Orientation.haut);
+					break;
+				case gauche:
+					r.setO(Orientation.bas);
+					break;
+		}
+	  }
+   }
 }
