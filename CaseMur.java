2599dcc28775543f99e248052d0fb533da137913
ThomasAyr <99038943+ThomasAyr@users.noreply.github.com>
2022-04-01 16:27:12 +0200

    Update CaseMur.java

--- CaseMur.java
+++ CaseMur.java
@@ -12,16 +12,16 @@ public class CaseMur extends Case {
 
 
         case haut:
-            r.setY(r.getY() -1 );
+            r.setX(r.getX() +1 );
             break;
         case bas:
-            r.setY(r.getY() +1 );
-            break;
-        case droite:
             r.setX(r.getX() -1 );
             break;
+        case droite:
+            r.setY(r.getY() -1 );
+            break;
         case gauche:
-            r.setX(r.getX() + 1);
+            r.setY(r.getY() + 1);
             break;}
         // deux robots ne peuvent pas etre sur la même case
         // si le robot sort de la matrice théorique, il meurt...
