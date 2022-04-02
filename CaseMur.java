ef39d4145d3d185076d85ee87fb722f8021f7d77
ThomasAyr <99038943+ThomasAyr@users.noreply.github.com>
2022-04-02 13:46:21 +0200

    Update CaseMur.java

--- CaseMur.java
+++ CaseMur.java
@@ -1,29 +1,11 @@
-public class CaseMur extends Case {
-
-
-    public String getNom() {
-
-    return  "murH";
-    }
-
-    public void effet(Bot r) {
-
-        switch(r.getO()) {
-
-
-        case haut:
-            r.setX(r.getX() +1 );
-            break;
-        case bas:
-            r.setX(r.getX() -1 );
-            break;
-        case droite:
-            r.setY(r.getY() -1 );
-            break;
-        case gauche:
-            r.setY(r.getY() + 1);
-            break;}
-        // deux robots ne peuvent pas etre sur la même case
-        // si le robot sort de la matrice théorique, il meurt...
+public class CaseMur extends Case{
+	private String nom = "murH";
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void effet(Bot r) {
+		r.deplacementInverse();
     }
 }
