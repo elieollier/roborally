55f935c481a9d8d6a9f707829252b31bea55fb7d
ThomasAyr <99038943+ThomasAyr@users.noreply.github.com>
2022-04-07 23:33:19 +0200

    Update CaseLaser.java

--- CaseLaser.java
+++ CaseLaser.java
@@ -1,6 +1,15 @@
-public class CaseLaser extends Case {
+public class CaseLaser implements Case {
 	private String nom = "laser";
+	private String description = 
+			"Ci-contre vous trouverez l'image qui représente les cases Laser.\n"
+			+ "Les cases Laser, sont des cases qui tueront automatiquement votre\n"
+			+ "robot, il perdra donc une vie immédiatement s'il va sur ces cases,\n"
+			+ "évitez-les donc !\n";
 	
+	public String getDescription() {
+		return description;
+	}
+		
 	public String getNom() {
 		return nom;
 	}
