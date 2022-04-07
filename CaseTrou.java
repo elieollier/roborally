640d4b32dcf4efba51efa44f2d060f0c42b136bf
ThomasAyr <99038943+ThomasAyr@users.noreply.github.com>
2022-04-07 23:41:48 +0200

    Update CaseTrou.java

--- CaseTrou.java
+++ CaseTrou.java
@@ -1,5 +1,14 @@
-public class CaseTrou extends Case {
+public class CaseTrou implements Case {
 	private String nom = "trou";
+	private String description = 
+			"Ci-contre vous trouverez l'image qui représente les cases Trous.\n"
+			+ "Les cases Trous sont des cases qui tueront immédiatement votre\n"
+			+ "robot, il perdra donc une vie automatiquement s'il va sur ces\n"
+			+ "cases, ce sont donc des cases à éviter !";
+	
+	public String getDescription() {
+		return description;
+	}
 	
 	public String getNom() {
 		return nom;
