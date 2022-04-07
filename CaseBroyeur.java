d735f676b5fc07f68b18004907bee685e71f32d6
ThomasAyr <99038943+ThomasAyr@users.noreply.github.com>
2022-04-07 23:28:36 +0200

    Update CaseBroyeur.java

--- CaseBroyeur.java
+++ CaseBroyeur.java
@@ -1,10 +1,19 @@
-public class CaseBroyeur extends Case{
+public class CaseBroyeur implements Case {
 	private String nom = "broyeur";
+	private String description = 
+			"Ci-contre vous trouverez l'image qui représente les cases Broyeurs.\n"
+			+ "Les cases Broyeurs sont des cases qui tueront immédiatement votre robot,\n"
+			+ "il perdra donc une vie automatiquement s'il va sur ces cases,\n"
+			+ "ce sont donc des cases à éviter !";
 	
 	public String getNom() {
 		return nom;
 	}
 	
+	public String getDescription() {
+		return description;
+	}
+
 	public void effet(Bot r) {
 		r.mourir();
 	}
