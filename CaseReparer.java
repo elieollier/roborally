89650e5b5a2123589e59d91311e6f357c5a31c8b
ThomasAyr <99038943+ThomasAyr@users.noreply.github.com>
2022-04-07 23:38:21 +0200

    Update CaseReparer.java

--- CaseReparer.java
+++ CaseReparer.java
@@ -1,5 +1,13 @@
-public class CaseReparer extends Case{
+public class CaseReparer implements Case{
     private String nom = "reparer";
+	private String description = 
+			"Ci-contre vous trouverez l’image qui représente les cases de Réparation.\n"
+			+ "Les cases de Réparation, sont des cases qui réparent un des dégâts que\n"
+			+ "votre robot a subi, donc il peut y aller en toute tranquillité.";
+	
+	public String getDescription() {
+		return description;
+	}
     
     public String getNom() {
         return nom;
