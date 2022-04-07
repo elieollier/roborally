458927a9075f1c6d8fffb3fd2446bee2555e9602
ThomasAyr <99038943+ThomasAyr@users.noreply.github.com>
2022-04-07 23:34:44 +0200

    Update CaseMur.java

--- CaseMur.java
+++ CaseMur.java
@@ -1,5 +1,13 @@
-public class CaseMur extends Case{
+public class CaseMur implements Case{
 	private String nom = "murH";
+	private String description = 
+			"Ci-contre vous trouverez l'image qui représente les cases Mur.\n"
+			+ "Les cases Murs vous bloquent sur la case où est votre robot,"
+			+ "il est donc impossible d'aller sur une case Mur !\n";
+	
+	public String getDescription() {
+		return description;
+	}
 	
 	public String getNom() {
 		return nom;
