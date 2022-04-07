3ce8712f6063f5c662860b3edb7c603093a31266
ThomasAyr <99038943+ThomasAyr@users.noreply.github.com>
2022-04-07 23:43:26 +0200

    Update CaseNormale.java

--- CaseNormale.java
+++ CaseNormale.java
@@ -2,7 +2,7 @@ public class CaseNormale implements Case{
 	private String nom = "normale";
 	private String description = 
 			"Ci-contre vous trouverez l'image qui représente les cases normales.\n"
-			+ "Les cases Normales sont des cases où votre robot ne risque rie\n"
+			+ "Les cases Normales sont des cases où votre robot ne risque rien\n"
 			+ "pour sa vie, il peut donc y aller en toute tranquillité.";
 	
 	public String getDescription() {
