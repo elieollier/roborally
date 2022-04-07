8560be8f98ad2a1b4526c4ee6c2a93a03a5955de
ThomasAyr <99038943+ThomasAyr@users.noreply.github.com>
2022-04-07 23:40:02 +0200

    Update CaseTapisRoulant.java

--- CaseTapisRoulant.java
+++ CaseTapisRoulant.java
@@ -1,8 +1,17 @@
-public class CaseTapisRoulant extends Case {
+public class CaseTapisRoulant implements Case {
 	private Orientation o ;
-	
+	private String description =
+			"Ci-contre vous trouverez l'image qui représente les cases Tapis roulant.\n"
+			+ "Les cases Tapis roulants, vous permettent d'avancer d'une case suivant\n"
+			+ "le sens du tapis tout en conservant l'orientation de votre robot, vous\n"
+			+ "pouvez donc les utiliser mais attention à où ils vont amener votre robot !";
+
 	public String getNom() {
 		return "tapisroulant"+String.valueOf(o);
+	}
+	
+	public String getDescription() {
+		return description;
 	}
 	
 	public CaseTapisRoulant(Orientation o) {
